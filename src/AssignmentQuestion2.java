157eb4e148912b42861bf021e0f12e41ba5bf30a
Inderjit57 <singhinderjit57@yahoo.com>
2022-01-03 15:47:03 -0500

    Assignment IfElse

--- src/AssignmentQuestion2.java
+++ src/AssignmentQuestion2.java
@@ -2,7 +2,37 @@
 public class AssignmentQuestion2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int age = 23;
+		String level1 = "g1";
+		String level2 = "g2";
+		String level3 = "g";
+
+		int diff = 16 - age;
+
+		boolean iflevel1 = false;
+		boolean iflevel2 = true;
+		boolean iflevel3 = true;
+		boolean alllevel = (iflevel1 == iflevel2) && (iflevel1 == iflevel3) && (iflevel2 == iflevel3);
+
+		if (age < 16) {
+			System.out.print("minimum required age is 16, come back after: " + diff + "years");
+		} else if (age >= 16 && iflevel1) {
+			System.out.println("at level1: " + level1);
+			System.out.println("eligible to get g2: " + level2);
+		}
+
+		else if (age >= 16 && iflevel2) {
+			System.out.println("at level1: " + level2);
+			System.out.println("eligible to get g: " + level3);
+		}
+
+		else if (iflevel3) {
+			System.out.println("you have passed level3: " + level3);
+		}
+
+		else if (alllevel) {
+			System.out.println("all level passed");
+		}
 
 	}
 
--- src/AssignmentQuestion5.java
+++ src/AssignmentQuestion5.java
@@ -2,7 +2,14 @@
 public class AssignmentQuestion5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		double goal50 = 0.01; // 1% of commision for 50% goals
+		double goalLess70 = 0.05; //5%of commision for less than 70% goals achieved
+		double goalMore70 = 0.1; //10% of commision for more than 70% goals achieved
+		
+		boolean commision = true;
+		
+	
 
 	}
 
